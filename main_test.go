cafd5a03684b55ec828e361e5ba1ec7fe4ae1083
pauline <pauljdavi.t21@btinternet.com>
2024-06-08 18:08:22 +0100

    add failing test

--- main_test.go
+++ main_test.go
@@ -9,10 +9,19 @@ func TestCalculatePacks(t *testing.T) {
     }{
         {1, map[int]int{250: 1}},
         {250, map[int]int{250: 1}},
-        {251, map[int]int{250: 2}},
+        {251, map[int]int{500: 1}},
         {501, map[int]int{500: 1, 250: 1}},
         {12001, map[int]int{5000: 2, 2000: 1, 250: 1}},
     }
 
-
+    for _, tc := range testCases {
+        t.Run("OrderSize"+string(tc.orderSize), func(t *testing.T) {
+            packs := calculatePacks(tc.orderSize)
+            for size, count := range tc.expectedPacks {
+                if packs[size] != count {
+                    t.Errorf("For order size %d, expected %d packs of size %d, got %d", tc.orderSize, count, size, packs[size])
+                }
+            }
+        })
+    }
 }
